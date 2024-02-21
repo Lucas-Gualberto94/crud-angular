b9168c29a733ed2721e52e13d3fedb9fb7185759
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2024-02-20 21:40:26 -0300

    adding services e pipes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,15 +4,29 @@ import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Course } from './model/course';
 import { CoursesService } from './services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
+import { ErrorDialogComponent } from '../components/error-dialog/error-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { CategoryPipe } from '../pipes/category.pipe';
 
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule, MatCardModule, MatToolbarModule, CoursesComponent, MatProgressSpinnerModule, CommonModule],
+  imports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    CoursesComponent,
+    MatProgressSpinnerModule,
+    CommonModule,
+    ErrorDialogComponent,
+    MatIconModule,
+    CategoryPipe
+  ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
@@ -21,7 +35,22 @@ export default class CoursesComponent {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'categoria'];
 
-  constructor (private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
-   }
+  constructor (
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.');
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
 }
